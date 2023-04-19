4e56506b614cafc2322c2845a0a78cf2e40e3362
CJ301648X <CJ301648X@LAB01-PC37>
2023-04-19 12:54:54 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaPR2/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaPR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exec 1
+
+            //int num1;
+
+            //Console.WriteLine("Digite um nº inteiro: ");
+            //num1 = int.Parse(Console.ReadLine());
+
+
+            //if (num1 < 0)
+            //    num1 = -num1;
+
+            //Console.WriteLine("Seu valor absoluto é: " + num1);
+
+
+
+            //Exec 2
+
+            //int num;
+            //Console.WriteLine("Digite um nº inteiro: ");
+            //num = int.Parse(Console.ReadLine());
+
+            //if (num % 3 == 0 && num % 5 == 0)
+            //    Console.WriteLine("O nº é multiplo de 3 e 5");
+
+            //else if (num % 4 == 0 && num % 7 == 0)
+            //    Console.WriteLine("O nº é multiplo de 4 e 7");
+
+            //else if (num % 3 == 0)
+            //    Console.WriteLine("O nº é divisivel por 3");
+
+            //else if (num % 4 == 0)
+            //    Console.WriteLine("O n° é divisivel por 4");
+
+            //else if (num % 5 == 0)
+            //    Console.WriteLine("O nº é divisivel por 5");
+
+            //else if (num % 7 == 0)
+            //    Console.WriteLine("O nº é divisivel por 7");
+
+            //else
+            //{
+            //    Console.WriteLine("O nº não é divisivel por 3, 4, 5, 5");
+            //}
+
+
+            //Exec 3
+
+            //int num1, num2, num3;
+
+            //Console.WriteLine("digite o primeiro nº inteiro: ");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o segundo nº inteiro: ");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o segundo nº inteiro: ");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //2º parte
+            //{
+            //    if (num1 > num2 && num1 > num3)
+            //        Console.WriteLine("O maior nº inteiro é: " + num1);
+
+
+            //    if (num2 > num1 && num2 > num3)
+            //        Console.WriteLine("O maior nº inteiro é: " + num2);
+
+
+            //    else if (num3 > num1 && num3 > num2)
+            //        Console.WriteLine("O maior nº inteiro é: " + num3);
+
+            //Exec 4 
+
+            //int dia;
+
+            //Console.WriteLine("Digite um numero de 1 a 7: ");
+            //dia = int.Parse(Console.ReadLine());
+
+            //switch (dia)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Domingo: Dia de descanso e lazer");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("Disciplinas de segunda: História, Matemática e Português");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("Disciplinas de terça: Quimica, Inglês e Filosofia");
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("Disciplinas de quarta: Biologia, português e Programação");
+            //        break;
+
+            //    case 5:
+            //        Console.WriteLine("Disciplina de quinta: Sociologia, Redes de Computadores e Fisica");
+            //        break;
+
+            //    case 6:
+            //        Console.WriteLine("Disciplinas de sexta: Geografia, Matemática, Banco de dados");
+            //        break;
+
+            //    case 7:
+            //        Console.WriteLine("Sábado: Academia e outros afazeres");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Número invalido. Digite um número de 1 a 7.");
+            //            break;
+
+            //Exec 5 ERROOOOO
+
+            //int num, maior, menor;
+            //bool primeiro = true;
+
+            //Console.WriteLine("\nDigite uma sequencia de numeros inteiros:  \nCaso queira cancelar, digite 0\n");
+            //num = int.Parse(Console.ReadLine());
+            //maior = menor = num;
+
+            //while (num != 0)
+            //{
+            //    if (primeiro)
+            //    {
+            //        primeiro = false;
+
+            //    }
+            //    else
+            //    {
+            //        if (num > maior)
+            //        {
+            //            maior = num;
+
+            //        }
+            //        else if (num < menor)
+            //        {
+            //            menor = num;
+
+            //            num = int.Parse(Console.ReadLine());
+
+            //            if (primeiro)
+            //            {
+            //                Console.WriteLine("Nenhum número foi digitado!!");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("O maior nº da sequencia é: " + maior);
+            //                {
+            //                    Console.WriteLine("O menor nº da sequencia é: " + menor);
+
+
+            //Exec 6
+
+            //int num, somaDivi = 0;
+            //Console.Write("Digite um número inteiro: ");
+            //num = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i < num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        somaDivi += i;
+            //    }
+            //}
+        
+            //if (somaDivi == num)
+            //{
+            //    Console.WriteLine("O número é um número perfeito: " + num);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número não é um número perfeito: " + num);
+
+
+            }
+        }
+    }
+}
+
+
+
